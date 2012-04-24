dd4d034da625975b9c11909d752de4652776b825
Oliver Burger <obgr_seneca@mageia.org>
2012-04-24 12:56:56 +0200

    added gpl header

--- SystemCall.py
+++ SystemCall.py
@@ -1,3 +1,20 @@
+# Copyright (c) 2012 Oliver Burger obgr_seneca@mageia.org
+#
+# This file is part of videoconvert.
+#
+# videoconvert is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# videoconvert is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with videoconvert.  If not, see <http://www.gnu.org/licenses/>.
+
 class SystemCall():
     import subprocess as sp
     import os
